95b72ff7e4ec4108355e06ddbc4f497da8572a39
박수현 <sh@immerse.co.kr>
2024-06-28 08:44:42 +0900

    tailwind 설치

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="flex items-center justify-between">
+        <div>123</div>
+        <div>456</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
